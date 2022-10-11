f0f8a6213cc36d5576160fe5d0ae2ff3c023c306
Martin A. Garcia <82984398+martinagustin@users.noreply.github.com>
2022-10-11 18:32:42 -0300

    Add files via upload
    archivo definitivo, falta realizar mejoras porque tiene ciertas dependencias como:
    -se necesita que mobil y laptop esten en la misma red ya que el script abre el whatsappweb desde la laptop.
    -se debe de automatizar segun S.O para que se envie solo, esto implica que el dispositivo ssiempre este conectado.

--- script-what.py
+++ script-what.py
@@ -1,12 +1,27 @@
-#import pywhatkit
-import time
+import pywhatkit
 from datetime import datetime
-today = datetime.today()
 #print(today)
-fecha_hora_str = today.strftime('%d/%m/%Y %H:%M')
-actual="enviado: "+fecha_hora_str
-print(actual)
-# Send a WhatsApp Message to a Contact at 1:30 PM
-#pywhatkit.sendwhatmsg_instantly("+543487662839", "Hola")
-#pywhatkit.sendwhatmsg_instantly("+543487347519", "hello", 5, False)
-#pywhatkit.sendwhatmsg("+3487347519", "Enviado desde python a las ", 13, 30)
+fecha_actual = datetime.today()
+fecha_hora_str = fecha_actual.strftime('%y/%m/%d')
+hoy= datetime.now().strftime('%Y-%m-%d')
+#navidad
+navidad = datetime.strptime("2022-12-25", "%Y-%m-%d")
+restanteNavidad=navidad-fecha_actual
+#anio nuevo
+anioNuevo = datetime.strptime("2022-12-31", "%Y-%m-%d")
+restanteAnioNuevo=anioNuevo-fecha_actual
+#Viaje
+viaje = datetime.strptime("2023-03-02", "%Y-%m-%d")
+restanteviaje=viaje-fecha_actual
+#creamos lista con los numeros a enviar el mensaje
+listaNumeros=["+54xxxxxxxxxx","+54xxxxxxxxxx"]
+
+#aca esta el mensaje definitivo dentro de un string
+mensaje='---Faltan---\n-\t{} dias para navidad\n-\t{} dias para anionuevo\n-\t{} dias para el viaje'.format(restanteNavidad.days, restanteAnioNuevo.days,restanteviaje.days)
+print(mensaje)
+#creamos bucle for para que envie ambos mensajes 
+
+for i in range(2):
+    pywhatkit.sendwhatmsg_instantly(listaNumeros[i], mensaje, 10)
+    print('mensaje enviado a {}'.format(listaNumeros[i]))
+
